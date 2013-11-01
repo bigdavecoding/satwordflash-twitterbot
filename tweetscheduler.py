2c058eb5f54c5470bede107458720580d1cc83b8
cheungd <xihua.cheung@gmail.com>
2013-11-01 12:54:40 -0400

    11/01/2013 - DELETE unused job schedule

--- tweetscheduler.py
+++ tweetscheduler.py
@@ -9,10 +9,6 @@ def timed_job():
     print 'This job is run every minute.'
     bot.send_tweet()
 
-@sched.cron_schedule(day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print 'This job is run every weekday at 5pm.'
-
 sched.start()
 
 while True:
