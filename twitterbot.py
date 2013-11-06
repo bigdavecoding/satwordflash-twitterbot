deff1824004012c20cff5185b3e6a77dbb53b85c
David Cheung <xihua.cheung@gmail.com>
2013-11-05 23:05:23 -0500

    Log Word tweeted using mongodb as the data store

--- twitterbot.py
+++ twitterbot.py
@@ -4,8 +4,10 @@
 import bitly_api
 import random
 import json
+import base64
 import ConfigParser as cp
 from twython import Twython
+from pymongo import MongoClient
 
 class TwitterBot(object):
 
@@ -22,7 +24,11 @@ class TwitterBot(object):
         self.consumer_key = config.get("TWITTER", "CONSUMER_KEY")
         self.consumer_secret = config.get("TWITTER", "CONSUMER_SECRET")
         self.token = config.get("TWITTER", "TOKEN")
-        self.token_secret = config.get("TWITTER", "TOKEN_SECRET")        
+        self.token_secret = config.get("TWITTER", "TOKEN_SECRET")
+        self.mongo_url = base64.b64decode(config.get("MONGODB", "URL")).decode('ascii')
+        self.mongo_db = config.get("MONGODB", "DATABASE")
+        self.mongo_user = base64.b64decode(config.get("MONGODB", "USERNAME")).decode('ascii')
+        self.mongo_pass = base64.b64decode(config.get("MONGODB", "PASSWORD")).decode('ascii')
     
     #bitly api - shorten url
     def shorten_url(self, a_url, access_token):
@@ -30,7 +36,14 @@ class TwitterBot(object):
         data = conn.shorten(a_url)
         shortened_url = data['url']
         return shortened_url
-        
+
+    def log_word(self, word):
+        mongo_client = MongoClient(self.mongo_url)
+        db = mongo_client[self.mongo_db]
+        db.authenticate(self.mongo_user, self.mongo_pass)
+        word_activity_log = db["word_activity"]
+        word_activity_log.insert({"word": word})
+
     def send_tweet(self):        
         #get random word from the word list
         rand_index = random.randint(0, len(self.word_list))
@@ -47,4 +60,5 @@ class TwitterBot(object):
         #tweet message
         twitter = Twython(self.consumer_key, self.consumer_secret, self.token, self.token_secret)
         twitter.update_status(status=msg)
+        self.log_word(word)
         print(msg)
